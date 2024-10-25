69d77062ba531e649a149b57cbf8a40cb249262c
Totilla <tsm.antonia@gmail.com>
2024-10-25 10:19:21 +0200

    correo de verdad

--- data/config_plantilla.php
+++ data/config_plantilla.php
@@ -4,3 +4,8 @@ define('DB_HOST', '');
 define('DB_USER', '');
 define('DB_PASS', '');
 define('DB_NAME', '');
+
+//credenciales para el envío de correos
+define('MAIL_HOST', '');
+define('MAIL_USER', '');
+define('MAIL_PASS', '');
